3022a1cd9b3919c453ccb4340dafe58d88d6ce47
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2024-07-30 22:28:28 -0300

    Cron to restart server

--- src/cron.ts
+++ src/cron.ts
@@ -1,23 +1,28 @@
-import cron from "cron";
+import { CronJob } from "cron";
 import https from "https";
 import { env } from "./env";
 
-const job = new cron.CronJob("*/14 * * * *", () => {
-  console.log("Restarting server...");
+const job = new CronJob(
+  "*/14 * * * *",
+  () => {
+    console.log("Restarting server...");
 
-  https
-    .get(env.RENDER_EXTERNAL_URL || env.API_BASE_URL, (res) => {
-      if (res.statusCode === 200) {
-        console.log("Server restarted!");
-      } else {
-        console.log(
-          `Failed to restart server with status code: ${res.statusCode}`
-        );
-      }
-    })
-    .on("error", (err) => {
-      console.log("Error during restart:", err.message);
-    });
-});
+    https
+      .get(env.RENDER_EXTERNAL_URL || env.API_BASE_URL, (res) => {
+        if (res.statusCode === 200) {
+          console.log("Server restarted!");
+        } else {
+          console.log(
+            `Failed to restart server with status code: ${res.statusCode}`
+          );
+        }
+      })
+      .on("error", (err) => {
+        console.log("Error during restart:", err.message);
+      });
+  },
+  null,
+  true
+);
 
 export { job };
